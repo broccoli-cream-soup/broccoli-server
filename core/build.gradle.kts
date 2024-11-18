89b1aec424a8765d5913140a821d7ab8955aa2ca
jombi <jombi@duck.com>
2024-11-18 11:18:09 +0900

    build :: add mongo to save dynamic fields

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
+    api("org.springframework.boot:spring-boot-starter-data-mongodb")
+
     api(project(":business"))
     api(project(":common"))
 }
