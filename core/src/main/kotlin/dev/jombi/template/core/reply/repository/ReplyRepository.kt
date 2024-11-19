db09baad7dbb2d687bd7103c89a168fc055d7576
jombi <jombi@duck.com>
2024-11-19 09:40:36 +0900

    feat :: reply repository

---
+++ core/src/main/kotlin/dev/jombi/template/core/reply/repository/ReplyRepository.kt
@@ -0,0 +1,9 @@
+package dev.jombi.template.core.reply.repository
+
+import dev.jombi.template.core.reply.entity.Reply
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface ReplyRepository : MongoRepository<Reply, ObjectId> {
+    fun findRepliesByUserIdIs(userId: Long): List<Reply>
+}
