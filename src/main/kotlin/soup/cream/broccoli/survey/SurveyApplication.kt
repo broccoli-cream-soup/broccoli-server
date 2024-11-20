c81fb25136b0b0577319c720273f6d4d1d2c3653
jombi <jombi@duck.com>
2024-11-20 12:15:00 +0900

    chore :: change Name

--- src/main/kotlin/soup/cream/broccoli/survey/SurveyApplication.kt
+++ src/main/kotlin/soup/cream/broccoli/survey/SurveyApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringTemplatesApplication
+class SurveyApplication
 
 fun main(args: Array<String>) {
-    runApplication<SpringTemplatesApplication>(*args)
+    runApplication<SurveyApplication>(*args)
 }
